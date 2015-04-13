35de1939f813749bf54c398158845bc6439ac8c9
gcoenraets <gilles_coenraets@msn.com>
2015-04-13 15:50:24 +0200

    Use string data

--- src/main/scala/risk/Main.scala
+++ src/main/scala/risk/Main.scala
@@ -2,7 +2,13 @@ package risk
 
 object Main {
   def main(args: Array[String]) {
-    val risk = new SyntheticRisk("""D:\Users\gcoenrae\Downloads\Nav.csv""")
+    val risk = new SyntheticRisk("""20010103;100
+20010104;102
+20010105;101
+20010112;103
+20010119;105
+20010126;104
+20010202;108""")
     println(risk.toString())
   }
 }
